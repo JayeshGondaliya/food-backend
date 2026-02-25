618627ecd53a1dbbdc7da53aad7439f21d278043
Jayesh Gondaliya <166483981+JayeshGondaliya@users.noreply.github.com>
2026-02-25 22:37:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const io = new Server(server, {
 });
 
 // CORS middleware – must come before routes
-app.use(cors({ origin: 'https://food-backend-ak09.onrender.com' }));
+app.use(cors({ origin: 'https://food-frontend-er67.vercel.app' }));
 app.use(express.json());
 
 // Make io accessible in controllers
